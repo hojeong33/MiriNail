dc29aa28206ceda641d514c65dd3bec34161720c
hojeong <jhj20071@naver.com>
2022-04-15 13:26:26 +0900

    style:navbar 초안

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
-import Community from './routes/Community/Community';
-import Main from './routes/Main/Main';
-import Mypage from './routes/Mypage/Mypage';
-import Tutorial from './routes/Guide/Tutorial';
-import GlobalStyle from './styles/global';
-import FAQ from './routes/Guide/FAQ';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Community from "./routes/Community/Community";
+import Main from "./routes/Main/Main";
+import Mypage from "./routes/Mypage/Mypage";
+import Tutorial from "./routes/Guide/Tutorial";
+import GlobalStyle from "./styles/global";
+import FAQ from "./routes/Guide/FAQ";
 
 function App() {
   return (
@@ -16,11 +16,11 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Main />}/>
-          <Route path="/mypage" element={<Mypage />}/>
-          <Route path="/Community" element={<Community />}/>
-          <Route path="/tutorial" element={<Tutorial />}/>
-          <Route path="/faq" element={<FAQ />}/>
+          <Route path="/" element={<Main />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/Community" element={<Community />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/faq" element={<FAQ />} />
         </Routes>
         <Footer />
       </BrowserRouter>
