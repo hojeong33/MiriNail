5129bb5d37882d8aad94b589b3efec077c671ef9
FreeBono <rhkrehd619@naver.com>
2022-05-06 21:52:29 +0900

    #S06P31E101-373 Feat: AI DETECTION VIDEO ON WEB(rev)

--- AI/main.py
+++ AI/main.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from flasktest import get_stream_video
+# from flasktest import get_stream_video
 from testtemp3guhyun import testVideo
 from matplotlib import pyplot as plt
 import tensorflow as tf
