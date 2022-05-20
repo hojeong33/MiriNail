dab4446e645cb526f392169276837fd0dd56b78c
최호준 <matt9504@naver.com>
2022-05-20 11:48:45 +0900

    Merge : for master

--- AI/main.py
+++ AI/main.py
@@ -48,7 +48,6 @@ def video_streaming():
 
 @app.get("/nail")
 def read_root():
-    # print('음... 이ㅐ건떠야하는데')
     return {"Hello": "World"}
 
 class Item(BaseModel):
@@ -70,16 +69,10 @@ async def streamer(gen):
         cv2.destroyAllWindows()
         cv2.VideoCapture(0, cv2.CAP_DSHOW).release() 
         WebcamVideoStream(src=0).stop()
-        # print('되기는 하는건가?')
-        # print(cv2.VideoCapture.isOpend())
-        # print("caught cancelled error")
 
 
 @app.get("/nail/video")
 def main():
-    # StringResponse함수를 return하고,
-    # 인자로 OpenCV에서 가져온 "바이트"이미지와 type을 명시
-    # print('video url 접근')
     return StreamingResponse(streamer(video_streaming()), media_type="multipart/x-mixed-replace; boundary=frame")
 
 @app.get("/nail/items/{item_id}")
@@ -103,13 +96,10 @@ async def client(request: Request):
 # 웹소켓 설정 ws://127.0.0.1:8000/ws 로 접속할 수 있음
 @app.websocket("/websocket")
 async def websocket_endpoint(websocket: WebSocket):
-    # print(f"client connected : {websocket.client}")
     response = requests.get(f'https://k6e101.p.ssafy.io/api/nailart/detail/{test}')
-    # print(response.content)
     print(test)
     nailImage = response.json()['nailartImgUrl']
     print(nailImage)
-    # print(response.text)
     await websocket.accept() # client의 websocket접속 허용
     await websocket.send_text(f"Welcome client : {websocket.client}")
     i = 0
@@ -117,18 +107,13 @@ async def websocket_endpoint(websocket: WebSocket):
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_hands = mp.solutions.hands
-    # print(f'{mp_hands} 일단 선언?')
     args = {
         "model": "./model/export_model_008/frozen_inference_graph.pb",
-        # "model":"/media/todd/38714CA0C89E958E/147/yl_tmp/readingbook/model/export_model_015/frozen_inference_graph.pb",
         "labels": "./record/classes.pbtxt",
-        # "labels":"record/classes.pbtxt" ,
         "num_classes": 1,
         "min_confidence": 0.6,
         "class_model": "../model/class_model/p_class_model_1552620432_.h5"}
-    # print(args)
     COLORS = np.random.uniform(0, 255, size=(args["num_classes"], 3))
-    # print(COLORS)
     print('1 : 모델 생성')
     model = tf.Graph()
     with model.as_default():
@@ -195,18 +180,11 @@ async def websocket_endpoint(websocket: WebSocket):
                     # print('여기까진 옴')
                     if results.multi_hand_landmarks:
                         for num, hand in enumerate(results.multi_hand_landmarks):
-                            # print(mp_hands.HandLandmark.MIDDLE_FINGER_TIP.name)
-                            # print(type(mp_hands.HandLandmark.MIDDLE_FINGER_TIP.value))
-                            # if (mp_hands.HandLandmark.MIDDLE_FINGER_TIP.value) == 12:
-                            #     print('와 드디어 들어오나????????')
                             normalizedLandmark = hand.landmark[12]
                             normalizedLandmark_2 = hand.landmark[11]
                             pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, imageWidth, imageHeight)
                             pixelCoordinatesLandmark_2 = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark_2.x, normalizedLandmark_2.y, imageWidth, imageHeight)
-                            # print('12번픽셀 좌표 : ', pixelCoordinatesLandmark)
-                            # print('11번픽셀 좌표 : ', pixelCoordinatesLandmark_2)
-                            # print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-                            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
                             try:
                                 tanTheta = ((pixelCoordinatesLandmark_2[0]-pixelCoordinatesLandmark[0]))/((pixelCoordinatesLandmark_2[1]-pixelCoordinatesLandmark[1]))
                                 theta = np.arctan(tanTheta)
@@ -214,25 +192,7 @@ async def websocket_endpoint(websocket: WebSocket):
                                 angle = theta*180/math.pi
                             except:
                                 print('수평')
-                            # print('12번 nomalized 좌표 : ', normalizedLandmark)
-                            # print(normalizedLandmark)
-                            # for point in mp_hands.HandLandmark:
-                            #     if point == 'HandLandmark.MIDDLE_FINGER_TIP':
-                            #         print('아니이거 맞지않나????????????stx????????????????????????')
-                            #     normalizedLandmark = hand.landmark[point]
-                            #     pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, imageWidth, imageHeight)
-                                
-                            #     print(point)
-                            #     # print('으아ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ',hand.landmark[12])
-                            #     print(pixelCoordinatesLandmark)
-                            #     print(normalizedLandmark)
 
-                            # print(results.multi_hand_landmarks)
-                            # mp_drawing.draw_landmarks(image_2,hand,mp_hands.HAND_CONNECTIONS,
-                            # mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                            # mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2),
-                            #     )
-                    # cv2.imshow('Output',image_2)
 
                             (boxes, scores, labels, N) = sess.run(
                                 [boxesTensor, scoresTensor, classesTensor, numDetections],
@@ -242,10 +202,8 @@ async def websocket_endpoint(websocket: WebSocket):
                             labels = np.squeeze(labels)
                             boxnum = 0
                             box_mid = (0, 0)
-                            # print("scores_shape:", scores.shape)
-                            # frame[ 200:200+img_height , 200:200+img_width ] = img
+
                             for (box, score, label) in zip(boxes, scores, labels):
-                                # print(int(label))
                                 # if int(label) != 1:
                                 #     continue
                                 if score < args["min_confidence"]:
@@ -287,11 +245,9 @@ async def websocket_endpoint(websocket: WebSocket):
                                 he = int(abs(endX-startX))
                                 try: 
                                     cat_sticker = cv2.resize(img,(wi,he))
-                                    # print(cat_sticker.shape)
 
                                     # 회전
                                     img_rotate = ff.rotate_image(cat_sticker,angle)
-                                    # print('회전된 이미지 shape : ' ,img_rotate.shape)
                                 except:
                                     print('에러')
 
@@ -309,40 +265,6 @@ async def websocket_endpoint(websocket: WebSocket):
                                 except :
                                     print('에러')
 
-                                # try :
-                                #     sticker_area= image_2[yy:yy+cat_sticker.shape[0],xx:xx+cat_sticker.shape[1]]
-                                #     image_2[yy:yy+cat_sticker.shape[0], xx:xx+cat_sticker.shape[1]] = np.where(cat_sticker==0,sticker_area,cat_sticker).astype(np.uint8)
-                                # except :
-                                #     print('에러')
-
-                                
-                            # show the image_2 image
-                            # print(boxnum)
-                            # if box_mid == (0, 0):
-                            #     drawboxes.clear()
-                            #     cv2.putText(image_2, 'Nothing', (20, 50),
-                            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
-                            # elif boxnum == 1:
-                            #     drawboxes.append(box_mid)
-                            #     if len(drawboxes) == 1:
-                            #         pp = drawboxes[0]
-                            #         cv2.circle(image_2, pp, 0, (0, 0, 0), thickness=3)
-                            #         # cv2.line(image_2, pt1, pt2, (0, 0, 0), 2, 2)
-                            #     if len(drawboxes) > 1:
-                            #         num_p = len(drawboxes)
-                            #         for i in range(1, num_p):
-                            #             pt1 = drawboxes[i - 1]
-                            #             pt2 = drawboxes[i]
-                            #             # cv2.circle(image_2, pp, 0, (0, 0, 0), thickness=3)
-                            #             cv2.line(image_2, pt1, pt2, (0, 0, 0), 2, 2)
-                                        
-                            #     cv2.putText(image_2, 'Point', (20, 50),
-                            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
-                            # else:
-                            #     drawboxes.clear()
-                            #     cv2.putText(image_2, 'Nothing', (20, 50),
-                            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
-
 
                     
                     # i += 1
@@ -352,10 +274,7 @@ async def websocket_endpoint(websocket: WebSocket):
                     # frame을 byte로 변경 후 특정 식??으로 변환 후에
                     # yield로 하나씩 넘겨준다.
                     image_2 = buffer.tobytes()
-                    # print(buffer.tobytes())
-                 
-                    # print(f"message received : {image_2} from : {websocket.client}")
-                    # print(image_2)
+
                     # yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
                     # bytearray(image_2) + b'\r\n')
                     # senddata =(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
